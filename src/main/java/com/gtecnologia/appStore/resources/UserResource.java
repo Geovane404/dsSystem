4ae901273ab75b29873f98dbf4c2f60cb11b16a3
Geovane Calheira <geovanecalheiro@gmail.com>
2022-01-30 10:54:53 -0400

    Service layer and component registration

--- src/main/java/com/gtecnologia/appStore/resources/UserResource.java
+++ src/main/java/com/gtecnologia/appStore/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.gtecnologia.appStore.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gtecnologia.appStore.entities.User;
+import com.gtecnologia.appStore.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> finAll() {
-		User n1 = new User(1L, "Maria", "maria@gmail.com", "90909090", "1234567");
-		User n2 = new User(2L, "Alex", "alex@gmail.com", "90909090", "1234567");
-		User n3 = new User(3L, "Jose", "jose@gmail.com", "90909090", "1234567");
-		return ResponseEntity.ok().body(n1);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+
+	@GetMapping(value = "/{id}")
+	ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
