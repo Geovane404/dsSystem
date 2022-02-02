fc7621ca9fe6ac3fdc80340f08eb6f5ce7bc4f1e
Geovane Calheira <geovanecalheiro@gmail.com>
2022-02-02 08:38:17 -0400

    Exception handling - findById

---
+++ src/main/java/com/gtecnologia/appStore/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.gtecnologia.appStore.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.gtecnologia.appStore.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resouceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Recurso não encontrado";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+
+		StandardError resp = new StandardError();
+		resp.setTimestamp(Instant.now());
+		resp.setStatus(status.value());
+		resp.setError(error);
+		resp.setMessage(e.getMessage());
+		resp.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(resp);
+	}
+
+}
